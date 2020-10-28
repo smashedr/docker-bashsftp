feceeaebc06f39261ad216af8a85b0e0535b1a7b
Shane <shane@shanerice.me>
2020-10-27 20:41:21 -0700

    ship it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,9 @@ if [[ -z "${SFTP_PASS}" ]];then
 fi
 
 [[ -z "${SFTP_USER}" ]] && SFTP_USER="ftpuser"
-[[ -z "${SFTP_HOME}" ]] && SFTP_HOME="/data"
 [[ -z "${SFTP_PORT}" ]] && SFTP_PORT="2222"
+
+[[ -z "${HTML_ROOT}" ]] && HTML_ROOT="/data"
 
 if [[ ! -f /etc/ssh/ssh_host_rsa_key ]]; then
     ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ''
@@ -25,8 +26,8 @@ if [[ "${SFTP_PORT}" != "2222" ]];then
     sed -i "s/Port 2222/Port ${SFTP_PORT}/" /etc/ssh/sshd_config
 fi
 
-if ! id -u "${SFTP_HOME}" >/dev/null 2>&1;then
-    adduser -D -H -h "${SFTP_HOME}" -G root "${SFTP_USER}"
+if ! id -u "${SFTP_USER}" >/dev/null 2>&1;then
+    adduser -u 1000 -D -H -h "${HTML_ROOT}" -G root "${SFTP_USER}"
 fi
 if [[ "${SFTP_PASS:0:3}" = '$6$' ]];then
     sed -i "s/${SFTP_USER}:!/${SFTP_USER}:${SFTP_PASS}/" /etc/shadow
